5aaf727033d418a51fe25ed155da707147d3ec95
Wellignton Souza <wellignton.ssouzaa@gmail.com>
2023-08-22 19:57:08 -0300

    First entity (User) and its test rest controller

---
+++ src/main/java/com/aulaspring/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.aulaspring.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aulaspring.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "91955555555", "123456789");
+        return ResponseEntity.ok().body(u);
+    }
+}
