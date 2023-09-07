fa5d362d596cc039991f5d90f0530d980d79fa0f
Wellignton Souza <wellignton.ssouzaa@gmail.com>
2023-09-07 12:35:46 -0300

    Adição do atributo orderStatus em Order e correção do Data seeding

--- src/main/java/com/aulaspring/course/entities/Order.java
+++ src/main/java/com/aulaspring/course/entities/Order.java
@@ -3,6 +3,7 @@ package com.aulaspring.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.aulaspring.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -27,6 +28,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant instant;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -35,9 +38,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant instant, User client) {
+    public Order(Long id, Instant instant, OrderStatus orderStatus, User client) {
         this.id = id;
         this.instant = instant;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -56,6 +60,16 @@ public class Order implements Serializable {
 
     public void setInstant(Instant instant) {
         this.instant = instant;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
