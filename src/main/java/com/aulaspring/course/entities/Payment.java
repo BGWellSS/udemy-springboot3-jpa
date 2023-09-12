13e441048b1eb2597a4db4dafbd5630d31e4d73f
Wellignton Souza <wellignton.ssouzaa@gmail.com>
2023-09-12 18:03:15 -0300

    JsonIgnore em order(Payment) - correção do loop

--- src/main/java/com/aulaspring/course/entities/Payment.java
+++ src/main/java/com/aulaspring/course/entities/Payment.java
@@ -3,6 +3,8 @@ package com.aulaspring.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,6 +27,7 @@ public class Payment implements Serializable {
 
   @OneToOne
   @MapsId
+  @JsonIgnore
   private Order order;
 
   // Construtores
